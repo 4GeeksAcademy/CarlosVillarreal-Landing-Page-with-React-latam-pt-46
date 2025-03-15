fadc3cc5ee4186303280f8d938eb4420af5582dc
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-03-15 03:59:42 +0000

    avance

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,12 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import NavBar from './components/Home';
+import Hero from './components/Hero'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <NavBar/>
+    <Hero/>
   </React.StrictMode>,
 )
