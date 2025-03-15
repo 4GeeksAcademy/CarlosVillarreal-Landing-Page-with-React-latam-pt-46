81c024d725c4d9b0157a1e96f47a6e916c5c570a
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-03-15 04:22:41 +0000

    ajustando el contenido del home

---
+++ src/js/components/Cards.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Cards = () => {
+    return (
+
+<div class="card grid grid-cols-1 md:grid-cols-4 gap-4 p-4">
+  <img src="https://picsum.photos/300/200" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Call action</a>
+  </div>
+</div>
+    );
+};
+
+
+export default Cards;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,12 +9,19 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import NavBar from './components/Home';
-import Hero from './components/Hero'
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Cards from './components/Cards';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NavBar/>
-    <Hero/>
+    <Home/>
+    <Navbar/>
+    <Banner/>
+    <div>
+    <Cards/>    <Cards/>    <Cards/>    <Cards/>
+    </div>
+   
   </React.StrictMode>,
 )
