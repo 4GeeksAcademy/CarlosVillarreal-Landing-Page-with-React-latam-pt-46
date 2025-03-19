f24f541267a429bb078fc881ae515086522003d1
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-03-19 01:37:52 +0000

    el proyecto ya lo tenía listo, tenía que desactivar el autoguardado para verlo

--- src/js/components/Banner.jsx
+++ src/js/components/Banner.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react';
 
 const Banner = () => {
-    return (
-
-        <div className="card" style={{width: "18rem;"}}>
-                <div className="card-body">
-                    <h5 className="card-title fs-1">A Warm Welcome!</h5>
-                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipsicing elit, Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quareat nam nobis illo aspernatur vitae fugiat numquam repellat</p>
-                    <a href="#" className="btn btn-primary">Call to action! </a>
-                </div>
-        </div>
-
-    );
+  return (
+    <div className="jumbotron text-left m-4 rounded">
+      <h1 className="display-3">A Warm Welcome!</h1>
+      <p className="lead">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam,
+        eligendi, in quo sunt possimus non incidunt odit vero aliquid similiqu
+        e quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.
+      </p>
+      <a href="#" className="btn btn-primary fs-4">
+        Call to action!
+      </a>
+    </div>
+  );
 };
 
 export default Banner;
